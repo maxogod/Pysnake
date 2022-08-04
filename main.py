3b75e0a9be8e4b35e6cc7acef0808c33f1de9073
maxogod <maxi18wars@gmail.com>
2022-08-04 01:23:09 -0300

    beta

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ class Snake:
     def __init__(self, parent_screen, length) -> None:
         self.parent_screen = parent_screen
         self.snake_block = pygame.image.load("resources/snake_body.jpg").convert()
+        self.snake_head = pygame.image.load("resources/snake_head.jpg").convert()
         self.length = length
         self.x = [SIZE] * self.length
         self.y = [SIZE] * self.length
@@ -15,9 +16,9 @@ class Snake:
 	
     def draw(self):
         self.parent_screen.fill((37, 171, 123))
-        for i in range(self.length):
+        self.parent_screen.blit(self.snake_head, (self.x[0], self.y[0]))
+        for i in range(1, self.length):
             self.parent_screen.blit(self.snake_block, (self.x[i], self.y[i]))
-        pygame.display.flip()
         
     def move(self, direction):
         self.direction = direction
@@ -27,7 +28,7 @@ class Snake:
     def walk(self):
         directionsy = {'up': - SIZE, 'down': SIZE}
         directionsx = {'right': SIZE, 'left': - SIZE}
-        for i in range(self.length-1,0,-1):
+        for i in range(self.length-1, 0, -1):
             self.x[i] = self.x[i-1]
             self.y[i] = self.y[i-1]
         if self.walk_direction in directionsx:
@@ -45,12 +46,21 @@ class Snake:
 class Apple:
     def __init__(self, parent_screen) -> None:
         self.parent_screen = parent_screen
-        self.apple = pygame.image.load("resources/apple.jpg").convert()
+        self.apple = pygame.image.load("resources/apple.png").convert_alpha()
         self.x = random.randint(0, 18) * SIZE
         self.y = random.randint(0, 13) * SIZE
     
     def draw(self):
         self.parent_screen.blit(self.apple, (self.x, self.y))
+        
+    def change_pos(self, snake_x, snake_y):
+        new_x = random.randint(0, 18) * SIZE
+        new_y = random.randint(0, 13) * SIZE
+        while new_x in snake_x or new_y in snake_y:
+            new_x = random.randint(0, 18) * SIZE
+            new_y = random.randint(0, 13) * SIZE
+        self.x = new_x
+        self.y = new_y
 
 
 class Game:
@@ -58,14 +68,25 @@ class Game:
         pygame.init()
         self.screen = pygame.display.set_mode((800, 600))
         self.screen.fill((37, 171, 123))
-        self.snake = Snake(self.screen, 5)
+        self.snake = Snake(self.screen, 1)
         self.snake.draw()
         self.apple = Apple(self.screen)
         self.apple.draw()
         
     def run(self):
         running = True
+        start = True
         while running:
+            if start:
+                self.title()
+                while not (pygame.key.get_pressed()[K_ESCAPE] or pygame.key.get_pressed()[K_KP_ENTER]
+                           or not start):
+                    for event in pygame.event.get():
+                        if event.type == QUIT or pygame.key.get_pressed()[K_ESCAPE]:
+                            exit(0)
+                        elif pygame.key.get_pressed()[K_SPACE]:
+                            start = False
+                        
             for event in pygame.event.get():
                 if event.type == QUIT or pygame.key.get_pressed()[K_ESCAPE]:
                     running = False
@@ -73,14 +94,62 @@ class Game:
                     for i in [K_UP, K_DOWN, K_RIGHT, K_LEFT]:
                         if pygame.key.get_pressed()[i] and not game.check_180_degrees(i):
                             self.snake.move(i)
-            if (self.snake.x[0] >= 800 or self.snake.x[0] < 0) or (self.snake.y[0] >= 600 or self.snake.y[0] < 0):
-                running = False
+                            
             self.snake.walk()
+            if not self.snake_collision_check():
+                hscore = self.highscore_check()
+                self.gameover(hscore)
+                while not (pygame.key.get_pressed()[K_ESCAPE] or pygame.key.get_pressed()[K_SPACE]
+                           or event.type == QUIT):
+                    for event in pygame.event.get():
+                        if event.type == QUIT or pygame.key.get_pressed()[K_ESCAPE]:
+                            exit(0)
+                        elif pygame.key.get_pressed()[K_SPACE]:
+                            self.reinitialize()
+                            
             self.apple_collision_check()
             self.apple.draw()
             self.score()
             pygame.display.flip()
             time.sleep(.09)
+    
+    def title(self):
+        self.title_img = pygame.image.load("resources/title.jpg").convert()
+        self.screen.blit(self.title_img, (0, 0))
+        pygame.display.flip()
+        
+    def reinitialize(self):
+        self.screen = pygame.display.set_mode((800, 600))
+        self.screen.fill((37, 171, 123))
+        self.snake = Snake(self.screen, 1)
+        self.snake.draw()
+        self.apple = Apple(self.screen)
+        self.apple.draw()
+    
+    def gameover(self, hscore):
+        self.dead = pygame.image.load("resources/dead.jpg").convert()
+        font = pygame.font.SysFont('', 40)
+        
+        score = font.render(f'Your score: {self.snake.length}', True, (255, 255, 255))
+        hscore_text = font.render(f'Highscore: {hscore}', True, (255, 255, 255))
+        
+        self.screen.blit(self.dead, (0, 0))
+        self.screen.blit(score, (350, 320))
+        self.screen.blit(hscore_text, (350, 380))
+        pygame.display.flip()
+        
+    def highscore_check(self):
+        score = self.snake.length
+        with open('resources/highscore.csv', 'r') as hscore:
+            try:
+               hscore_content = int(hscore.readline().rstrip('\n'))
+            except ValueError:
+                hscore_content = 0
+        if score > hscore_content:
+            with open('resources/highscore.csv', 'w') as hscore:
+                hscore.write(str(score))
+                return score
+        return hscore_content
     
     def check_180_degrees(self, key):
         boolean = False
@@ -94,20 +163,29 @@ class Game:
             boolean = True            
         return boolean
     
-    def snake_collosion_check(self):
-        #TODO
-        pass
+    def snake_collision_check(self):
+        boolean = True
+        if self.snake.length > 1:
+            pos = 1
+            while pos < self.snake.length:
+                if self.snake.x[0] == self.snake.x[pos] and self.snake.y[0] == self.snake.y[pos]:
+                    boolean = False
+                pos += 1
+                
+        if boolean and (self.snake.x[0] >= 800 or self.snake.x[0] < 0) or (self.snake.y[0] >= 600 or self.snake.y[0] < 0):
+            boolean = False
+        return boolean
     
     def apple_collision_check(self):
         if self.snake.x[0] == self.apple.x and self.snake.y[0] == self.apple.y:
             self.snake.increase_length()
+            self.apple.change_pos(self.snake.x, self.snake.y)
     
     def score(self):
         font = pygame.font.SysFont('arial', 40)
         score = font.render(f'Score: {self.snake.length}', True, (255, 255, 255))
         self.screen.blit(score, (650, 550))
      
-    
 
 if __name__ == '__main__':
     game = Game()
